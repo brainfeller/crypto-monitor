0889eb57de1e1db2001bc6a97ecf3962a826a3bb
brainfeller <fellerok@gmail.com>
2022-09-27 20:41:19 +0200

    crytov1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:crypto_test/screens/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Crypto monitor',
+      home: MainPage(),
+    );
+  }
+}
+
